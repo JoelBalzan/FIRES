fix noistks calculation

src/FIRES/functions/genfrb_parallel.py
@@ -24,8 +24,8 @@ def process_dynspec_with_pa_rms(dynspec, frequency_mhz_array, time_ms_array, rm)
     tsdata, _, _, noistks = process_dynspec(
         dynspec, frequency_mhz_array, time_ms_array, rm)
     
-    tsdata.phits[tsdata.iquvt[0] < 10.0 * noistks[0]] = np.nan
-    tsdata.dphits[tsdata.iquvt[0] < 10.0 * noistks[0]] = np.nan
+    tsdata.phits[tsdata.iquvt[0]  < noistks[0]] = np.nan
+    tsdata.dphits[tsdata.iquvt[0] < noistks[0]] = np.nan
 
     # Filter data to include only from the peak time onwards
     peak_time_index = np.argmax(tsdata.iquvt[0], axis=0)  # Find the peak index

src/FIRES/functions/plotfns.py
@@ -203,7 +203,7 @@ def plot_ilv_pa_ds(sc_dspec, freq_mhz, time_ms, save, fname, outdir, tsdata, noi
 			- tsdata: Time series data object
 			- noistks: Noise levels for each Stokes parameter
 	"""
-	# Apply zooming
+
 	iquvt  = tsdata.iquvt
 	phits  = tsdata.phits
 	dphits = tsdata.dphits
@@ -214,10 +214,10 @@ def plot_ilv_pa_ds(sc_dspec, freq_mhz, time_ms, save, fname, outdir, tsdata, noi
 	pfrac  = tsdata.pfrac
 	epfrac = tsdata.epfrac
 	
-	phits[iquvt[0] < 10.0 * noistks[0]] = np.nan
-	dphits[iquvt[0] < 10.0 * noistks[0]] = np.nan
+	#phits[iquvt[0]  < noistks[0]] = np.nan
+	#dphits[iquvt[0] < noistks[0]] = np.nan
 
-	igood = np.where(iquvt[0] > 10.0 * noistks[0])[0]
+	igood = np.where(iquvt[0] > noistks[0])[0]
 	
 	lmax = np.argmax(lfrac[igood])
 	vmax = np.argmax(vfrac[igood])