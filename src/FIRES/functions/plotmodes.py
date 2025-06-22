c2b84f13fd5c17216e8ead74e97df4ed7b7d6270
JoelBalzan <19011859@student.westernsydney.edu.au>
2025-06-22 13:51:11 +0800

    propagate change simulated frb tuple changes, add snr

--- src/FIRES/functions/plotmodes.py
+++ src/FIRES/functions/plotmodes.py
@@ -82,29 +82,31 @@ class PlotMode:
 		
 def basic_plots(fname, frb_data, mode, gdict, out_dir, save, figsize, tau_ms, show_plots):
 
-	ds_data = frb_data
+	dspec_params = frb_data.dspec_params
+	freq_mhz = dspec_params.freq_mhz
+	time_ms = dspec_params.time_ms
+	
 
 	ts_data, corr_dspec, noise_spec, noise_stokes = process_dynspec(
-		ds_data.dynamic_spectrum, ds_data.freq_mhz, ds_data.time_ms, gdict, tau_ms
+		frb_data.dynamic_spectrum, freq_mhz, time_ms, gdict, tau_ms
 	)
 
 	iquvt = ts_data.iquvt
-	time_ms = ds_data.time_ms
-	freq_mhz = ds_data.freq_mhz
-
+	snr = frb_data.snr
+	
 	if mode == "all":
-		plot_ilv_pa_ds(corr_dspec, freq_mhz, time_ms, save, fname, out_dir, ts_data, figsize, tau_ms, show_plots)
+		plot_ilv_pa_ds(corr_dspec, freq_mhz, time_ms, save, fname, out_dir, ts_data, figsize, tau_ms, show_plots, snr)
 		plot_stokes(fname, out_dir, corr_dspec, iquvt, freq_mhz, time_ms, save, figsize, show_plots)
 		plot_dpa(fname, out_dir, noise_stokes, ts_data, time_ms, 5, save, figsize, show_plots)
-		estimate_rm(ds_data.dynamic_spectrum, freq_mhz, time_ms, noise_spec, 1.0e3, 1.0, out_dir, save, show_plots)
+		estimate_rm(frb_data.dynamic_spectrum, freq_mhz, time_ms, noise_spec, 1.0e3, 1.0, out_dir, save, show_plots)
 	elif mode == "iquv":
 		plot_stokes(fname, out_dir, corr_dspec, iquvt, freq_mhz, time_ms, save, figsize, show_plots)
 	elif mode == "lvpa":
-		plot_ilv_pa_ds(corr_dspec, freq_mhz, time_ms, save, fname, out_dir, ts_data, figsize, tau_ms, show_plots)
+		plot_ilv_pa_ds(corr_dspec, freq_mhz, time_ms, save, fname, out_dir, ts_data, figsize, tau_ms, show_plots, snr)
 	elif mode == "dpa":
 		plot_dpa(fname, out_dir, noise_stokes, ts_data, time_ms, 5, save, figsize, show_plots)
 	elif mode == "RM":
-		estimate_rm(ds_data.dynamic_spectrum, freq_mhz, time_ms, noise_spec, 1.0e3, 1.0, out_dir, save, show_plots)
+		estimate_rm(frb_data.dynamic_spectrum, freq_mhz, time_ms, noise_spec, 1.0e3, 1.0, out_dir, save, show_plots)
 	else:
 		print(f"Invalid mode: {mode} \n")
 
