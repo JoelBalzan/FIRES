fee533c5646f35cac5e24eb120e6ef230d57f286
Joel <jbalzan.astro@gmail.com>
2025-11-04 10:55:56 +0800

    batch generate plots

---
+++ bash/pa.sh
@@ -0,0 +1,9 @@
+fires -f phase_paper_a1 -p pa_var --data . -s -o . --plot-scale log --figsize 8 6 -v --use-latex --compare-windows full:leading full:trailing full:total --weight-x-by width_ms --weight-y-by PA_i
+
+fires -f phase_paper_a1 -p pa_var --data . -s -o . --plot-scale linear --figsize 8 6 -v --use-latex --compare-windows full:leading full:trailing full:total --weight-x-by width_ms --weight-y-by PA_i
+
+fires -f freq_paper_a1 -p pa_var --data . -s -o . --plot-scale log --figsize 8 6 -v --use-latex --compare-windows 1q:total 4q:total full:total --weight-x-by width_ms --weight-y-by PA_i
+
+fires -f freq_paper_a1 -p pa_var --data . -s -o . --plot-scale linear --figsize 8 6 -v --use-latex --compare-windows 1q:total 4q:total full:total --weight-x-by width_ms --weight-y-by PA_i
+
+fires -f freq_phase_paper_a1 -p pa_var --data . -s -o . --plot-scale log --figsize 8 6 -v --use-latex --compare-windows 1q:total 4q:total full:total full:leading full:trailing full:total --weight-x-by width_ms --weight-y-by PA_i
