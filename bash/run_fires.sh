e395efeedff63c6629d24be35bf8e029f759c930
Joel <jbalzan.astro@gmail.com>
2025-11-04 10:24:11 +0800

    add bash scripts folder

---
+++ bash/run_fires.sh
@@ -0,0 +1,3 @@
+sbatch --job-name=fires_n10 --export=ALL,N=10 fires_n_loop.sh
+sbatch --job-name=fires_n100 --export=ALL,N=100 fires_n_loop.sh
+sbatch --job-name=fires_n1000 --export=ALL,N=1000 fires_n_loop.sh
