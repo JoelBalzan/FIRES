e85d0424eb3fdb8338909a384ac6ff8c1b29c7d3
Joel <jbalzan.astro@gmail.com>
2025-11-04 10:27:53 +0800

    update slurm scripts

--- bash/run_fires.sh
+++ bash/run_fires.sh
@@ -1,3 +1,4 @@
-sbatch --job-name=fires_n10 --export=ALL,N=10 fires_n_loop.sh
-sbatch --job-name=fires_n100 --export=ALL,N=100 fires_n_loop.sh
-sbatch --job-name=fires_n1000 --export=ALL,N=1000 fires_n_loop.sh
+sbatch --job-name=PA0 --export=ALL,PA_sd=0 fires_n_loop.sh
+sbatch --job-name=PA30 --export=ALL,PA_sd=30 fires_n_loop.sh
+sbatch --job-name=PA60 --export=ALL,PA_sd=60 fires_n_loop.sh
+sbatch --job-name=PA90 --export=ALL,PA_sd=90 fires_n_loop.sh
