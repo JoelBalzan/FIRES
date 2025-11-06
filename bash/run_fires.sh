4d1a7c37503e8183bb765b8627b6bf50ef15e587
Joel <jbalzan.astro@gmail.com>
2025-11-06 13:40:11 +0800

    override width

--- bash/run_fires.sh
+++ bash/run_fires.sh
@@ -1,4 +1,12 @@
-sbatch --job-name=PA0 --export=ALL,PA_sd=0 fires_n_loop.sh
-sbatch --job-name=PA30 --export=ALL,PA_sd=30 fires_n_loop.sh
-sbatch --job-name=PA60 --export=ALL,PA_sd=60 fires_n_loop.sh
-sbatch --job-name=PA90 --export=ALL,PA_sd=90 fires_n_loop.sh
+#sbatch --job-name=PA0 --export=ALL,PA_sd=0 fires_n_loop.sh
+#sbatch --job-name=PA30 --export=ALL,PA_sd=30 fires_n_loop.sh
+#sbatch --job-name=PA60 --export=ALL,PA_sd=60 fires_n_loop.sh
+#sbatch --job-name=PA90 --export=ALL,PA_sd=90 fires_n_loop.sh
+
+sbatch --job-name=10 --export=ALL,N=10,mg_width_low=1,mg_width_high=5 fires_n_loop.sh
+sbatch --job-name=100 --export=ALL,N=100,mg_width_low=1,mg_width_high=5 fires_n_loop.sh
+sbatch --job-name=600 --export=ALL,N=600,mg_width_low=1,mg_width_high=5 fires_n_loop.sh
+
+sbatch --job-name=10 --export=ALL,N=10,mg_width_low=20,mg_width_high=40 fires_n_loop.sh
+sbatch --job-name=100 --export=ALL,N=100,mg_width_low=20,mg_width_high=40 fires_n_loop.sh
+sbatch --job-name=600 --export=ALL,N=600,mg_width_low=20,mg_width_high=40 fires_n_loop.sh
