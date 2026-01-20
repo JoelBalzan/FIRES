845169a9c0fcb8dcef1efd4209308143ad7cd864
Joel <jbalzan.astro@gmail.com>
2026-01-20 14:54:10 +0800

    comment out linear pa trend removal

--- paper/240318A/make_lvpa.sh
+++ paper/240318A/make_lvpa.sh
@@ -4,8 +4,8 @@ fires -f 240318A -p lvpa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/2
 fires -f 240318A_nn -p lvpa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/240318A/ -v  --seed 0 --scint --override-param "tau=0.128" -o /home/joel/Documents/GitHub/FIRES/paper/240318A/
 fires -f 240318A_nn_ns -p lvpa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/240318A/ -v  --seed 0 --override-param "tau=0" -o /home/joel/Documents/GitHub/FIRES/paper/240318A/
 
-fires -f 240318A_sd10 -p pa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/240318A/ -v  --seed 0 --sefd 1.4 --scint --override-param "tau=0.128" -o /home/joel/Documents/GitHub/FIRES/paper/240318A/
-fires -f 240318A_sd10nn -p pa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/240318A/ -v  --seed 0 --scint --override-param "tau=0.128" -o /home/joel/Documents/GitHub/FIRES/paper/240318A/
+fires -f 240318A_sd10 -p pa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/240318A/ -v  --seed 0 --sefd 1.4 --scint --override-param "tau=0.128" -o /home/joel/Documents/GitHub/FIRES/paper/240318A/ --override-plot extension='jpg'
+fires -f 240318A_sd10nn -p pa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/240318A/ -v  --seed 0 --scint --override-param "tau=0.128" -o /home/joel/Documents/GitHub/FIRES/paper/240318A/ --override-plot extension='jpg'
 
 fires -f 240318A_sd0 -p pa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/240318A/ -v  --seed 0 --sefd 1.4 --scint --override-param "tau=0.128" "sd_PA=0" -o /home/joel/Documents/GitHub/FIRES/paper/240318A/
 fires -f 240318A_sd0nn -p pa --ncpu 12 --config-dir ~/Documents/GitHub/FIRES/paper/240318A/ -v  --seed 0 --scint --override-param "tau=0.128" "sd_PA=0" -o /home/joel/Documents/GitHub/FIRES/paper/240318A/
