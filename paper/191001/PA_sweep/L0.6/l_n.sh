b9dba553fd331909ca2607e735f9b86947ff2b26
Joel <jbalzan.astro@gmail.com>
2026-01-27 16:50:14 +0800

    paper examples

---
+++ paper/191001/PA_sweep/L0.6/l_n.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+fires -f phase_paper_lead_N_L0.6 -p l_frac -d /mnt/Storage/FIRES/191001/PA/Nsnr/n500/L0.6/N/ -o . -v --obs-data /mnt/Storage/craft_frb/191001 --obs-params /mnt/Storage/craft_frb/191001/parameters.txt --plot-config ./plotparams.toml --phase-window first --config-dir ~/Documents/GitHub/FIRES/paper/191001/
---
+++ paper/240318A/PA_sweep/L0.83/l_n.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+fires -f phase_paper_240318A -p l_frac -d /mnt/Storage/FIRES/240318A/PA/N/ -o . -v --obs-data /mnt/Storage/craft_frb/240318A/ --obs-params /mnt/Storage/craft_frb/240318A/parameters.txt --plot-config ./plotparams.toml --phase-window first
