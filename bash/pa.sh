6a25ff9d9de73c6966ef98cc227906ad7ceefebf
Joel <jbalzan.astro@gmail.com>
2026-01-27 16:53:31 +0800

    remove old bash scripts

--- bash/pa.sh
+++
@@ -1,9 +0,0 @@
-fires -f phase_paper_a1 -p pa_var --data . -s -o . --plot-scale log --figsize 8 6 -v --use-latex --compare-windows full:leading full:trailing full:total --weight-x-by width --weight-y-by PA_i
-
-fires -f phase_paper_a1 -p pa_var --data . -s -o . --plot-scale linear --figsize 8 6 -v --use-latex --compare-windows full:leading full:trailing full:total --weight-x-by width --weight-y-by PA_i
-
-fires -f freq_paper_a1 -p pa_var --data . -s -o . --plot-scale log --figsize 8 6 -v --use-latex --compare-windows 1q:total 4q:total full:total --weight-x-by width --weight-y-by PA_i
-
-fires -f freq_paper_a1 -p pa_var --data . -s -o . --plot-scale linear --figsize 8 6 -v --use-latex --compare-windows 1q:total 4q:total full:total --weight-x-by width --weight-y-by PA_i
-
-fires -f freq_phase_paper_a1 -p pa_var --data . -s -o . --plot-scale log --figsize 8 6 -v --use-latex --compare-windows 1q:total 4q:total full:total full:leading full:trailing full:total --weight-x-by width --weight-y-by PA_i
