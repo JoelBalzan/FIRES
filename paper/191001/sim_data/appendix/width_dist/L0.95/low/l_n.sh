816113092b7a43655cb20166aad03f0ece42f72d
Joel <jbalzan.astro@gmail.com>
2026-03-16 12:55:58 +0800

    paper sim data

---
+++ paper/191001/sim_data/appendix/width_dist/L0.95/low/l_n.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+fires -f lfrac_w1-5_191001 -p l_frac -d . -o . -v --obs-data /mnt/Storage/craft_frb/191001 --obs-params /mnt/Storage/craft_frb/191001/parameters.txt --plot-config ./plotparams.toml --phase-window first
---
+++ paper/240318A/sim_data/appendix/width_dist/L0.83/low/l_n.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+fires -f lfrac_w1-5_240318A -p l_frac -d /mnt/Storage/FIRES/240318A/PA/mg_low/N/ -o . -v --obs-data /mnt/Storage/craft_frb/240318A/ --obs-params /mnt/Storage/craft_frb/240318A/parameters.txt --plot-config ./plotparams.toml --phase-window first
